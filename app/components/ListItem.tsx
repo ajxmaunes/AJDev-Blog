d4f445d7c437740e8fe011826a5d5ac51cf9d0b9
ajxmaunes <aljunemaunes0@gmail.com>
2023-11-11 18:36:15 +0800

    update ui

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -17,7 +17,7 @@ export default function ListItem({ post }: Props) {
               <h5 className="mb-2 text-[20px] max-md:text-[18px] font-semibold tracking-tight text-gray-900 hover:text-blue-900">{title}</h5>
           </a>
           <p className="mb-5 text-[14px] max-md:text-[12px] font-normal text-gray-700 dark:text-gray-400">{formattedDate}</p>
-          <a href={`/posts/${id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#205295] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <a href={`/posts/${id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#205295] rounded-lg hover:bg-blue-800 focus:outline-none dark:hover:bg-blue-700">
               Read more
               <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
